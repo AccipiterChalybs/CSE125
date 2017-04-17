24923b8aef73a357c4def72df585c06280ff4d8e
Stephen Trinh <s6trinh@ucsd.edu>
2017-04-17 12:27:48 -0700

    camera lock in place to prevent over-rotations

--- scripts/game/RotateMouse.js
+++ scripts/game/RotateMouse.js
@@ -2,20 +2,25 @@
  * Created by Stephen on 4/15/2017.
  */
 
+let MAX_ANGLE = Math.PI/2;
+
 class RotateMouse extends Component{
   constructor(){
     super();
     this.componentType = "RotateMouse";
+    this.xAngle = 0;
+    this.yAngle = 0;
     this.sensitivity = 0.08;
   }
 
   update() {
     let dr = quat.create();
-    console.log(Input.getAxis('mouseHorizontal'));
-    quat.rotateY(dr, dr, Time.deltaTime * Input.getAxis('mouseHorizontal')
-      * this.sensitivity * -1);
-    quat.rotateX(dr, dr, Time.deltaTime * Input.getAxis('mouseVertical')
-      * this.sensitivity * -1);
-    this.transform.rotate(dr);
+    this.xAngle += Time.deltaTime * Input.getAxis('mouseVertical') * this.sensitivity * -1;
+    this.yAngle += Time.deltaTime * Input.getAxis('mouseHorizontal') * this.sensitivity * -1;
+    this.xAngle = (this.xAngle < MAX_ANGLE) ? this.xAngle : MAX_ANGLE;
+    this.xAngle = (this.xAngle > -MAX_ANGLE) ? this.xAngle : -MAX_ANGLE;
+    quat.rotateY(dr, dr, this.yAngle);
+    quat.rotateX(dr, dr, this.xAngle);
+    this.transform.setRotation(dr);
   }
 }
