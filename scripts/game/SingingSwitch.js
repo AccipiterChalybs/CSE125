92a8d42e321e2d8533d5cfde0f3354b42d7b0c77
Ara Jermakyan <ajermaky@ucsd.edu>
2017-06-05 14:39:19 -0700

    Fix bug with Scene Reloading

--- scripts/game/SingingSwitch.js
+++ scripts/game/SingingSwitch.js
@@ -19,13 +19,16 @@ class SingingSwitch extends Listenable {
 
   start() {
     this._lock = this.transform.gameObject.getComponent('Lock');
+    // Debug.log("INSIDE SINGING SWITCH START");
     for(let i = 0; i < this._unparsedEvents.length; ++i){
       this._events.push(GameObject.prototype.SerializeMap[this._unparsedEvents[i]].getComponent("Event"));
     }
+
   }
 
   updateComponent() {
     if(!(this._lock && this._lock !== null) || this._lock._unlocked ) {
+
       if (Time.time - this._lastSingTime >= this.timeBeforeLoss) {
         this._currentOutput = Utility.moveTowards(this._currentOutput, 0, this.lossRate * Time.deltaTime);
       } else {
@@ -42,7 +45,7 @@ class SingingSwitch extends Listenable {
 
     let singer = interactingObj.getComponent('Sing');
 
-    // console.log("player is interacting with me. ", this.gameObject);
+    // console.log("player is interacting with me. ", this.gameObject.name);
     if (singer && singer !== null) {
       this._lastSingTime = Time.time;
     }
