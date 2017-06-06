b529b8b37d357ed604f232b48b0db86b7952a091
arno gau <arno.gao@gmail.com>
2017-06-06 01:35:39 +0100

    ag: point lightssss

--- scripts/UI/mainpage.js
+++ scripts/UI/mainpage.js
@@ -8,7 +8,6 @@ function startGame() {
   document.getElementById("progress").style.visibility="visible";
   document.getElementById("mainMenu").style.visibility="hidden";
   initialize();
-  loadOptionsGraphics();
 }
 
 function nameEq(check, axis) {
@@ -83,16 +82,19 @@ function loadOptionsControls() {
   }
 }
 
-function togglePointShadows(e) {
+function toggleShadows(lightList, e) {
   e.stopPropagation();
   if (this.innerHTML === 'Off') {
     this.innerHTML = 'On';
+    for (let light of lightList) {
+      if (light) {
+        light.isShadowCaster = true;
+      }
+    }
   } else {
     this.innerHTML = 'Off';
-    const lightList = []
-    GameObject.prototype.SceneRoot.findComponents('Light', lightList);
     for (let light of lightList) {
-      if (light instanceof PointLight) {
+      if (light) {
         light.isShadowCaster = false;
       }
     }
@@ -102,12 +104,22 @@ function togglePointShadows(e) {
 function loadOptionsGraphics() {
   const graphics = $('#graphics');
   graphics.css('opacity', '1');
+
+  const lightList = [];
+  GameObject.prototype.SceneRoot.findComponents('Light', lightList);
+  const pointLightList =
+    lightList.map((l) => { return l instanceof PointLight; });
+
   const toggleBtn = graphics.find('.toggle');
-  toggleBtn[0].onclick = togglePointShadows;
+  toggleBtn[0].onclick = toggleShadows.bind(toggleBtn[0], pointLightList);
+
+  const slider = graphics.find('.slider');
+  slider[0].innerHTML = `${lightList.length}`;
 }
 
 function openOptionsModal() {
   $('#options').modal('show');
+  loadOptionsGraphics();
 }
 
 function closeOptionsModal() {
